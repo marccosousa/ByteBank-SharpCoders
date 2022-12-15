e6e2fb558f30a150e18d9225310e1e89b7906f7c
Marco Ribeiro <marcosousacontato@gmail.com>
2022-12-15 00:03:00 -0300

    Adicionando projeto, com opções 1, 2 e 3 implementadas

---
+++ ByteBank-SharpCoders-ws/Conta.cs
@@ -0,0 +1,27 @@
+﻿namespace ByteBank
+{
+    class Conta
+    {
+        public string Titular { get; set; }
+        public string Cpf { get; set; }
+        public double Saldo { get; private set; }
+        public int Id { get; set; }
+
+        public Conta()
+        {
+        }
+
+        public Conta(string titular, string cpf, double saldo, int id)
+        {
+            Titular = titular;
+            Cpf = cpf;
+            Saldo = saldo;
+            Id = id;
+        }
+
+        public override string ToString()
+        {
+            return $"Conta ID#{Id}\nTitular: {Titular}, CPF: {Cpf}, Saldo: {Saldo:F2}";
+        }
+    }
+}
---
+++ ByteBank-SharpCoders-ws/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int option;
+            List<Conta> contas = new List<Conta>();
+            int i = 0;
+            do
+            {
+                ShowMenu();
+                option = int.Parse(Console.ReadLine());
+                Console.WriteLine("--------------------");
+
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Opção [1]: ");
+                        Console.WriteLine($"Digite os dados da conta #{i + 1} :");
+                        Console.Write("Titular: ");
+                        string nome = Console.ReadLine();
+                        Console.Write("Digite o CPF: ");
+                        string cpf = Console.ReadLine();
+                        Console.Write("Digite o saldo: ");
+                        double saldo = double.Parse(Console.ReadLine());
+                        Console.Write("Digite o ID desejado: ");
+                        int id = int.Parse(Console.ReadLine());
+                        contas.Add(new Conta(nome, cpf, saldo, id));
+                        Console.WriteLine("Parabéns, bem-vindo!");
+                        i++;
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Opção [2]: ");
+                        Console.Write("Qual o ID da conta que você quer deletar? #");
+                        int procurarId = int.Parse(Console.ReadLine());
+                        int indexParaSerRemovido = contas.FindIndex(x => x.Id == procurarId);
+                        contas.RemoveAt(indexParaSerRemovido);
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Opção [3]: ");
+                        foreach (Conta c in contas)
+                        {
+                            Console.WriteLine(c.ToString());
+                            Console.WriteLine();
+                        }
+                        break;
+
+                }
+                Console.WriteLine("--------------------");
+
+            } while (option != 0);
+        }
+
+        static void ShowMenu()
+        {
+            Console.WriteLine("[1] - Inserir novo usuário");
+            Console.WriteLine("[2] - Deletar um usuário");
+            Console.WriteLine("[3] - Listar contas registradas");
+            Console.WriteLine("[4] - Detalhes de um usuário");
+            Console.WriteLine("[5] - Total armazenado no banco");
+            Console.WriteLine("[6] - Manipular conta");
+            Console.WriteLine("[0] - Sair do menu");
+            Console.Write("[X] - Digite a opção desejada: ");
+        }
+
+    }
+}
+
